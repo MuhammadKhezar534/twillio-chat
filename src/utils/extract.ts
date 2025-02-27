24d7c3a10f85595dcc4d9a3afa0703c64ba205bb
unknown <muhammadkhezar36@gmail.com>
2025-02-27 12:00:00 +0500

    removed new console statements

--- src/utils/extract.ts
+++ src/utils/extract.ts
@@ -1,14 +1,11 @@
  const messaging = firebase.messaging();
-  console.log({ messaging });
   const fcmToken = await messaging.getToken();
-  console.log({ fcmToken });
 
   if (!fcmToken) {
     console.log("FcmNotifications: can't get fcm token");
     return;
   }
 
-  console.log("FcmNotifications: got fcm token", fcmToken);
   convoClient.setPushRegistrationId("fcm", fcmToken);
   messaging.onMessage((payload) => {
     console.log("FcmNotifications: push received", payload);
