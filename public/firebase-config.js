44ec30f03a5d3824c2fb4d40664f3b1f442d8156
unknown <muhammadkhezar36@gmail.com>
2025-03-23 15:59:12 +0500

    first commit

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBnBN8lrPMiYAsVS_gwhZeieVOixiG1Oys",
+  authDomain: "yahuda-44efd.firebaseapp.com",
+  projectId: "yahuda-44efd",
+  storageBucket: "yahuda-44efd.appspot.com",
+  messagingSenderId: "35433545387",
+  appId: "1:35433545387:web:322ba61f931562ed53c1ee",
+  measurementId: "G-1FZ4RRS91C",
+};
